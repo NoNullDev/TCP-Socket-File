c7e58e01517c767a222bf6a50f42b385f09104cc
NullDev <97764442+NoNullDev@users.noreply.github.com>
2022-09-03 13:14:38 -0300

    Fazendo algumas melhorias no client.py
    Agora o cliente pode enviar os dados da imagem, como por exemplo:
    - tamanho em bytes
    - nome da imagem
    - extensão do arquivo
    
    Resolvido alguns bugs.

--- client.py
+++ client.py
@@ -1,21 +1,43 @@
 import socket
-HOST = 'coloca o ip do servidor' # Endereco IP do Servidor
-PORT = 5005 # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print ('Para sair use CTRL+X\n')
+import os
+import json
 
-imagem = open("imagem-cliente.png","rb")
+HOST = 'coloca o ip do servidor' # Endereco IP do Servidor
+PORT = 5000 # Porta que o Servidor está
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+dest = (HOST, PORT)
+
+tcp.connect(dest)
+
+image_fullname = 'imagem-cliente.png' 
+image_ext = image_fullname.split('.')[1] # extensão
+image_name = image_fullname.split('.')[0] # nome sem extensão
+image_size = os.stat(image_fullname).st_size
+
+imageDict = {
+	"nome" : image_name,
+	"size" : image_size,
+	"ext" : image_ext
+}
+
+#enviar dados da imagem pro servidor
+tcp.send(str(len(json.dumps(imageDict))).encode())
+tcp.send(json.dumps(imageDict).encode('utf-8'))
+
+print("size enviado = " + str(image_size))
+print("nome enviado = " + image_name)
+print("extensão enviada = " + image_ext)
+
+
+imagem = open(image_fullname,"rb")
 imagem_quadro = imagem.read(2048)
 while imagem_quadro:
-	tcp.send(imagem_quadro)
-	imagem_quadro = imagem.read(2048)
+     tcp.send(imagem_quadro)
+     imagem_quadro = imagem.read(2048)
 
-while msg != '\x18':
-     tcp.sendto(text, dest)
-     msg = input()
+
+imagem.close()
 tcp.close()
-
-
-
+input('Press ENTER to exit')
