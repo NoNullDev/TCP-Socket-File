729ff24ec042d50832e4c36d171348f9b95c75c0
NullDev <97764442+NoNullDev@users.noreply.github.com>
2022-09-03 13:16:06 -0300

    Fazendo algumas melhorias no server.py
    Agora o servidor recebe os dados da imagem e adiciona automaticamente a extensão recebida no nome do arquivo "imagem-server"
    Resolvido alguns bugs.

--- server.py
+++ server.py
@@ -1,30 +1,35 @@
 import socket
-HOST = 'coloca o ip do servidor'              # Endereco IP do Servidor
-PORT = 5005            # Porta que o Servidor esta
+import os
+import json
+
+HOST = 'coloca o ip do servidor' # Endereco IP do Servidor
+PORT = 5000 # Porta que o Servidor esta
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
 print("ativado")
-while True:
-    con, cliente = tcp.accept()
-    print ('Conectado por',cliente)
-    while True:
-        imagem  =  open("imagem-server.png", "wb")
-        
-        imagem_quadro = con.recv(2048)
 
-        while imagem_quadro:
-            imagem.write(imagem_quadro)
-            imagem_quadro = con.recv(2048)
-        imagem.close()
+con, cliente = tcp.accept()
+print ('Conectado por',cliente)
 
-        if not msg:
-            print("FIM - Conexao Perdida")
-            break
-        elif msg == "fim":
-            print("FIM - Cliente pediu para encerrar o programa")
-            break
-    print ('Finalizando conexao do cliente'), cliente
-    con.close()
-    
+length = int(con.recv(2).decode())
+imageDict = json.loads((con.recv(length)).decode('utf-8'))
+
+print("size recebido = " + str(imageDict["size"]))
+print("nome recebido = " + imageDict["nome"])
+print("extensão recebida = " + imageDict["ext"])
+
+
+imagem = open("imagem-server." + imageDict["ext"], "wb")
+
+imagem_quadro = con.recv(2048)
+while imagem_quadro:
+    imagem.write(imagem_quadro)
+    imagem_quadro = con.recv(2048)
+
+
+imagem.close()
+con.close()
+input('Press ENTER to exit')
